30d6ed38f18f48f3078527f76a51be9c8a478e6e
PigeonNerd <yingchaoliu2@gmail.com>
2013-03-29 15:41:31 -0400

    can lanch more workers now

--- src/myserver/master.cpp
+++ src/myserver/master.cpp
@@ -30,8 +30,8 @@ static struct Master_state {
   bool server_ready;
   int max_num_workers;
   int num_pending_client_requests;
+  int num_worker_nodes;
 
-  int num_idle_workers;
   std::vector<Worker_handle>cpu_workers_queue;
   std::vector<Worker_handle>disk_workers_queue;
   std::vector<Request_msg>cpu_waiting_queue;
@@ -51,31 +51,27 @@ void master_node_init(int max_workers, int& tick_period) {
   tick_period = 5;
   //printf("The maximum number of workers %d\n", max_workers);
   // HOW TO SET THIS NUMBER ?
-  //mstate.max_num_workers = max_workers;
-  mstate.max_num_workers = 2; 
-
+  mstate.max_num_workers = max_workers;
+  // initially, we only setup one workers 
+  mstate.num_worker_nodes = 0;
   mstate.num_pending_client_requests = 0;
   // used for debug
-  mstate.num_idle_workers = 0;
   
   // don't mark the server as ready until the server is ready to go.
   // This is actually when the first worker is up and running, not
   // when 'master_node_init' returnes
   mstate.server_ready = false;
-
-  // here we temporally request 3 worker nodes
-  for(int i = 0 ; i < 1; i++) {
-      int tag = random();
-      Request_msg req(tag);
-      char name[20];
-      sprintf(name, "my worker %d", i);
-      req.set_arg("name", name);
-      request_new_worker_node(req);
-  }
+  int tag = random();
+  Request_msg req(tag);
+  char name[20];
+  sprintf(name, "my worker %d", mstate.num_worker_nodes);
+  req.set_arg("name", name);
+  request_new_worker_node(req);
 }
 
 void handle_new_worker_online(Worker_handle worker_handle, int tag) {
 
+  mstate.num_worker_nodes ++;
   // we put new worker into the map to keep track of the status of each worker node
   workerInfo* worker_info = new workerInfo();
   worker_info->num_idle_cpu = 2;
@@ -90,8 +86,6 @@ void handle_new_worker_online(Worker_handle worker_handle, int tag) {
   mstate.cpu_workers_queue.push_back( worker_handle );
   mstate.disk_workers_queue.push_back( worker_handle );
   
-  mstate.num_idle_workers += 2;
-  //printf("Number of idle workers: %d\n",mstate.num_idle_workers);
   // Now that a worker is booted, let the system know the server is
   // ready to begin handling client requests.  The test harness will
   // now start its timers and start hitting your server with requests.
@@ -116,6 +110,7 @@ void handle_worker_response(Worker_handle worker_handle, const Response_msg& res
   if( isDiskRequestDone ) {
     if(mstate.disk_waiting_queue.size() == 0) {
         mstate.disk_workers_queue.push_back( worker_handle);
+        mstate.workersMap[worker_handle]->num_idle_disk++;
     }else {
         Request_msg thisRequest = mstate.disk_waiting_queue.front();
         send_request_to_worker( worker_handle, thisRequest);
@@ -128,8 +123,7 @@ void handle_worker_response(Worker_handle worker_handle, const Response_msg& res
   // here means we do not have more work right now
   if( mstate.cpu_waiting_queue.size() == 0) {
     mstate.cpu_workers_queue.push_back( worker_handle );
-    mstate.num_idle_workers++;
-    //printf("Number of idle workers: %d\n",mstate.num_idle_workers);
+    mstate.workersMap[worker_handle]->num_idle_cpu++;
   }else {
     mstate.num_pending_client_requests++;
     Request_msg thisRequest = mstate.cpu_waiting_queue.front();
@@ -166,6 +160,7 @@ void handle_client_request(Client_handle client_handle, const Request_msg& clien
       if( mstate.disk_workers_queue.size() != 0) {
         Worker_handle thisWorker = mstate.disk_workers_queue.front();
         send_request_to_worker(thisWorker, worker_req);
+        mstate.workersMap[thisWorker]->num_idle_disk--; 
         mstate.disk_workers_queue.erase(mstate.disk_workers_queue.begin());
       }else {
         mstate.disk_waiting_queue.push_back(worker_req);
@@ -173,20 +168,36 @@ void handle_client_request(Client_handle client_handle, const Request_msg& clien
     return;
   }
   // we run out of workers for cpu intensive work
-  if( mstate.num_pending_client_requests == mstate.max_num_workers) {
+  if( mstate.num_pending_client_requests == mstate.num_worker_nodes * 2) {
     mstate.cpu_waiting_queue.push_back(worker_req);
     return;
   }
   mstate.num_pending_client_requests++;
   Worker_handle thisWorker = mstate.cpu_workers_queue.front();
-  mstate.num_idle_workers--;
   send_request_to_worker(thisWorker, worker_req);
+  mstate.workersMap[thisWorker]->num_idle_cpu--; 
   mstate.cpu_workers_queue.erase(mstate.cpu_workers_queue.begin());
 }
 
+void print_worker_status() {
+    std::map<Worker_handle, workerInfo*>::iterator it; 
+    for(it = mstate.workersMap.begin(); it != mstate.workersMap.end(); it ++) {
+       printf("| cpu: %d, disk: %d", (it->second)->num_idle_cpu, (it->second)->num_idle_disk);
+    }
+    printf("\n");
+}
 
 void handle_tick() {
 
+  print_worker_status(); 
+  if( mstate.num_worker_nodes < mstate.max_num_workers){
+      int tag = random();
+      Request_msg req(tag);
+      char name[20];
+      sprintf(name, "my worker %d", mstate.num_worker_nodes);
+      req.set_arg("name", name);
+      request_new_worker_node(req);
+  }
   // TODO: you may wish to take action here.  This method is called at
   // fixed time intervals, according to how you set 'tick_period' in
   // 'master_node_init'.
